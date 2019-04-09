d5c723d9cd884e30b49344a5ca1ab4663c452a7a
Halli jaouad <hallijaouad@yahoo.fr>
2019-04-10 01:16:44 +0200

    New class  model for add attribut created_at and updated_at to table

---
+++ src/main/java/com/jhl/ipaiemanager/models/AuditModel.java
@@ -0,0 +1,30 @@
+package com.jhl.ipaiemanager.models;
+
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+public class AuditModel{
+	@Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updatedAt;
+}
