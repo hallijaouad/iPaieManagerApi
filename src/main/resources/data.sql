9d5e69cd44a7ad612e796ca15f6bb8358428d3b1
Jaouad HALLI <hallijaouad@yahoo.fr>
2019-04-19 15:41:57 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,10 @@ INSERT INTO roles(pk_role, refext, libelle) VALUES (1,'ROLE_ADMIN', 'Profil admi
 INSERT INTO roles(pk_role, refext, libelle) VALUES (2,'ROLE_USER', 'Profil utilisateur');
 
 --INITIALISATION TABLE UTILISATEURS
-INSERT INTO users(pk_user, email, password, nom, prenom, actif, created_at, updated_at) values (1, 'admin@gmail.com', 'admin', 'Halli', 'Jaouad', 1, NOW(), NOW());
-INSERT INTO users(pk_user, email, password, nom, prenom, actif, created_at, updated_at) values (2, 'hallisaad@gmail.com', 'saad', 'Halli', 'saad', 1, NOW(), NOW());
+-- admin
+-- user
+INSERT INTO users(pk_user, email, password, nom, prenom, actif, created_at, updated_at) values (1, 'admin@gmail.com', '$2a$10$PlUy9PfCzfWc77BGkYovze4enTleI844uJWxeZ2D3vFTPuDqK0hWu', 'Halli', 'Jaouad', 1, NOW(), NOW());
+INSERT INTO users(pk_user, email, password, nom, prenom, actif, created_at, updated_at) values (2, 'hallisaad@gmail.com', '$2a$10$MpPT049MKSMc6b3M25uJ4eb/1UkYD74Y/uJc5Y9Cm7kg.LJOWVPDy', 'Halli', 'saad', 1, NOW(), NOW());
 
 --TABLE DE JOINTURE ENTRE USERS ET ROLES
 --Admin user profil
