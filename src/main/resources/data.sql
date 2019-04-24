548ada9a780fb5ca342d2a9def5de49722d4b097
hallijaouad <hallijaouad@yahoo.fr>
2019-04-24 14:07:38 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,18 @@
 --INITIALISATION TABLE ROLE
-INSERT IGNORE INTO roles(pk_role, refext, libelle) VALUES (1,'ROLE_ADMIN', 'Profil administrateur');
-INSERT IGNORE INTO roles(pk_role, refext, libelle) VALUES (2,'ROLE_USER', 'Profil utilisateur');
+INSERT  INTO roles(pk_role, refext, libelle) VALUES (1,'ROLE_ADMIN', 'Profil administrateur');
+INSERT  INTO roles(pk_role, refext, libelle) VALUES (2,'ROLE_USER', 'Profil utilisateur');
 
 --INITIALISATION TABLE UTILISATEURS
 -- admin
 -- user
-INSERT IGNORE INTO users(pk_user, email, password, nom, prenom, actif, created_at, updated_at) values (1, 'admin@gmail.com', '$2a$10$PlUy9PfCzfWc77BGkYovze4enTleI844uJWxeZ2D3vFTPuDqK0hWu', 'Halli', 'Jaouad', 1, NOW(), NOW());
-INSERT IGNORE INTO users(pk_user, email, password, nom, prenom, actif, created_at, updated_at) values (2, 'hallisaad@gmail.com', '$2a$10$MpPT049MKSMc6b3M25uJ4eb/1UkYD74Y/uJc5Y9Cm7kg.LJOWVPDy', 'Halli', 'saad', 1, NOW(), NOW());
+INSERT  INTO users(pk_user, email, password, nom, prenom, actif, created_at, updated_at) values (1, 'admin@gmail.com', '$2a$10$PlUy9PfCzfWc77BGkYovze4enTleI844uJWxeZ2D3vFTPuDqK0hWu', 'Halli', 'Jaouad', 1, NOW(), NOW());
+INSERT  INTO users(pk_user, email, password, nom, prenom, actif, created_at, updated_at) values (2, 'hallisaad@gmail.com', '$2a$10$MpPT049MKSMc6b3M25uJ4eb/1UkYD74Y/uJc5Y9Cm7kg.LJOWVPDy', 'Halli', 'saad', 1, NOW(), NOW());
 
 --TABLE DE JOINTURE ENTRE USERS ET ROLES
 --Admin user profil
-INSERT IGNORE INTO users_roles(fk_user,fk_role) VALUES (1,1);
-INSERT IGNORE INTO users_roles(fk_user,fk_role) VALUES (1,2);
+INSERT  INTO users_roles(fk_user,fk_role) VALUES (1,1);
+INSERT  INTO users_roles(fk_user,fk_role) VALUES (1,2);
 --User profil
-INSERT IGNORE INTO users_roles(fk_user,fk_role) VALUES (2,2);
+INSERT  INTO users_roles(fk_user,fk_role) VALUES (2,2);
 
 COMMIT;
